0d879b5a78aca3bc5258659751b696964ed5f7c9
freedemons <freedemons@192.168.114.1>
2016-09-06 16:39:17 +0800

    第一次上传仓库以提供给团队成员一起使用

---
+++ xinwork/js/setting.js
@@ -0,0 +1,22 @@
+window.XW={
+	env:"production",
+	base:"http://120.24.36.146/xinworkapp/"
+}
+
+//添加下边线
+function smallLine(){
+	if(mui.os.ios){
+		var eleArr = ["header",".mui-table-view","form"];
+		for (var i=0;i<eleArr.length;i++) {
+			var leg = document.querySelectorAll(eleArr[i]).length;
+			if(leg>0){
+				for (var j=0;j<leg;j++) {
+					document.querySelectorAll(eleArr[i])[j].classList.add("lineclass-ios");
+				}
+			}
+		}
+	}
+}
+window.onload=function(){
+	smallLine();
+}
