7901bcc964b9b93a9115a0f506ac238832dfd22e
freedemons <646714507@qq.com>
2016-11-16 09:44:31 +0800

    优化分享操作

--- js/share.js
+++ js/share.js
@@ -1,3 +1,4 @@
+var shares = '';
 //判断是否授权
 function shareAction(s,msg) {
     console.log("分享操作：");
@@ -23,6 +24,7 @@ function shareMessage(s,msg){
         console.log( "分享到\""+s.description+"\"成功！ " );
     }, function(e){
         console.log( "分享到\""+s.description+"\"失败: "+e.code+" - "+e.message );
+        mui.toast( "分享到\""+s.description+"\"失败");
     } );
 }
 
@@ -31,12 +33,42 @@ function shareMessage(s,msg){
  */
 function updateSerivces(){
 	plus.share.getServices( function(s){
-		share={};
+		shares={};
 		for(var i in s){
 			var t=s[i];
-			share[t.id]=t;
+			shares[t.id]=t;
 		}
 	}, function(e){
 		console.log( "获取分享服务列表失败："+e.message );
 	} );
+}
+
+// 打开分享
+function shareShow(sTitle,sContent,sHref){
+	var shareItems = [];
+	var pic = ["_www/images/logo.png"],thu = ["_www/images/thumbs.png"];
+	updateSerivces();
+	mui('body').on('tap', '.mui-popover-action .shareItem', function() {
+	var ss = shares['weixin'];
+	ss&&ss.nativeClient&&shareItems.push({content:sContent,href:sHref,title:sTitle,pictures:pic,thumbs:thu,extra:{scene:'WXSceneSession'}});
+	ss&&ss.nativeClient&&shareItems.push({content:sContent,href:sHref,title:sTitle,pictures:pic,thumbs:thu,extra:{scene:'WXSceneTimeline'}});
+	ss = shares['qq'];
+	ss&&ss.nativeClient&&shareItems.push({content:sContent,href:sHref,title:sTitle,pictures:pic,thumbs:thu});
+		mui('#shareJob').popover('toggle');
+		if(shares['weixin'] && shares['qq']) {
+						switch(this.id) {
+							case 'weixin':
+								shareAction(shares['weixin'],shareItems[0]);
+								break;
+							case 'pengyouquan':
+								shareAction(shares['weixin'], shareItems[1]);
+								break;
+							case 'qq':
+								shareAction(shares['qq'], shareItems[2])
+								break;
+						}
+					} else {
+						mui.toast('系统发生错误，无法分享。');
+					}
+	})
 }
